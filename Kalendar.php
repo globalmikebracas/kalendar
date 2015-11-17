35e5c017280c962e5b99f5f35911fe7d616d3a60
Miguel Angel Bracamontes Alatriste <miguel.bracamontes@globaloxs.com>
2015-11-17 13:18:05 -0600

    Inicializacion de la extension yii2 kalendar

---
+++ Kalendar.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace globaloxs\kalendar;
+
+/**
+ * This is just an example.
+ */
+class Kalendar extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
